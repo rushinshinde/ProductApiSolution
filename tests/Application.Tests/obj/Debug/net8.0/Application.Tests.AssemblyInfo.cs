cbbe5df928d071a41058137b6c9ab537690d5be2
Rushikesh Shinde <110456956+rushinshinde@users.noreply.github.com>
2026-07-22 11:50:59 +0530

    Added all Packages

--- tests/Application.Tests/obj/Debug/net8.0/Application.Tests.AssemblyInfo.cs
+++ tests/Application.Tests/obj/Debug/net8.0/Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d45ef6867417ba29e130293ef760c14c9312974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196b2bde08bfd3f091771b926cbf96be08a12b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
